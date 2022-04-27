c1e1ac45964dd2544639417675ea40c0c76b130d
AhmedEaswy <elesawy325@gmail.com>
2022-04-27 19:42:17 +0200

    using handlebars

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const path = require('path');
 const express = require('express');
 
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-
 const app = express();
 
-const   adminRoutes = require('./routes/admin'),
+const   adminData = require('./routes/admin'),
         shopRoutes = require('./routes/shop'),
         rootDir = require('./helpers/path');
+
+app.engine('hbs', exphbs.engine({ 
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutDir: path.join(__dirname, 'views/layouts'),
+    partialsDir : path.join(__dirname, 'views/partials'),
+}));
+
+app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404')
 })
 
 app.listen(3000);
